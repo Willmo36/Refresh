15844e5f2e0d1c3843707ceadefc76884cf10b63
Max Willmott PCA <willmott@outlook.com>
2013-06-21 09:09:32 +0100

    Exported refresh.js

--- Refresh.js
+++ Refresh.js
@@ -1,38 +1,42 @@
-var count = 0,
-    files = [],
-    browsers = [],
-    fs = require('fs'),
-    io = require('socket.io').listen(30571);
+var refresh = function(){
+	var count = 0,
+		files = [],
+		browsers = [],
+		fs = require('fs'),
+		io = require('socket.io').listen(30571);
 
-process.argv.forEach(function(item,index)
-{
-    if (index > 1)
-        files.push(item);
-});
+	process.argv.forEach(function(item,index)
+	{
+		if (index > 1)
+			files.push(item);
+	});
 
-console.log("Watching...");
+	console.log("Watching...");
 
-for (var i = 0; i < files.length; i++)
-{
-    fs.watchFile(files[i], function () {
-        count++;
-        console.log(count + " refreshes.");
-        if (browsers.length > 0)
-        {
-            for (var s = 0; s < browsers.length; s++)
-            {
-                browsers[s].emit('refresh',{});
-            }
-        }
-    });
-}
+	for (var i = 0; i < files.length; i++)
+	{
+		fs.watchFile(files[i], function () {
+			count++;
+			console.log(count + " refreshes.");
+			if (browsers.length > 0)
+			{
+				for (var s = 0; s < browsers.length; s++)
+				{
+					browsers[s].emit('refresh',{});
+				}
+			}
+		});
+	}
 
-io.sockets.on('connection',function(socket){
-    browsers.push(socket);
-   console.log(browsers.length + ' browsers connected');
+	io.sockets.on('connection',function(socket){
+		browsers.push(socket);
+	   console.log(browsers.length + ' browsers connected');
 
-    socket.on('disconnect',function(){
-       browsers.splice(socket);
-       console.log(browsers.length + ' browsers connected');
-    });
-});
+		socket.on('disconnect',function(){
+		   browsers.splice(socket);
+		   console.log(browsers.length + ' browsers connected');
+		});
+	});
+};
+
+module.exports = refresh();
